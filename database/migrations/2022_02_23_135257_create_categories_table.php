099809a47f378a4b7dd11da7a871b152d871fdc2
Giovi-17 <giovanni.nuzzo17@gmail.com>
2022-02-23 16:03:09 +0100

    creazione e modifica post con aggiunta della categoria

--- database/migrations/2022_02_23_135257_create_categories_table.php
+++ database/migrations/2022_02_23_135257_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name',255);
-            $table->string('slug',255);
+            $table->string('name',20);
+            $table->string('slug',25)->unique();
             $table->timestamps();
         });
     }
