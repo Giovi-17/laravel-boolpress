36ac015ec845b91333d0ed3f85f616a54b401651
Giovi-17 <giovanni.nuzzo17@gmail.com>
2022-03-03 14:28:16 +0100

    Implementazione Vue Router e creazione di varie pagine

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,19 +1,27 @@
 <template>
-    <div>
 
-        <Posts />
+    <div>
+    
+        <Header />
+
+        <main>
+
+            <router-view></router-view>
+            
+        </main>
 
     </div>
+
 </template>
 
 <script>
-import Posts from '../components/Posts.vue'
+import Header from '../components/Header.vue';
 
 export default {
     name: 'App',
     components: {
 
-        Posts
+        Header
 
     }
 
